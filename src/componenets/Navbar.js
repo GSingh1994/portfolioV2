40c77dba34c76a559c84fa38e3831dff848b20b3
gsingh1994 <singh.gagandeep0034@gmail.com>
2021-04-23 22:45:14 -0500

    :sparkles: Add Hamburger-spring menu

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -1,8 +1,12 @@
+import React, { useState } from "react";
 import logo from "../assets/myLogo.svg";
-// import gitLogo from "../assets/logo-github.svg";
-// import mailLogo from "../assets/mail.svg";
-// import linkedinLogo from "../assets/logo-linkedin.svg";
+import "../styles/hamburger.scss";
+
 const Navbar = () => {
+  const [isActive, setActive] = useState(false);
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
   return (
     <div>
       <nav className="Navbar">
@@ -18,22 +22,21 @@ const Navbar = () => {
               <span>Resume</span>
             </button>
           </li>
-          {/* <li>
-            <a href="https://github.com/GSingh1994">
-              <img src={gitLogo} width="32px" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/gagandeep-singh1994/">
-              <img src={linkedinLogo} width="32px" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="mailto:singh.gagandeep0034@gmail.com">
-              <img src={mailLogo} width="32px" alt="" />
-            </a>
-          </li> */}
         </ul>
+        {/* Hamburger menu */}
+        <button
+          className={
+            isActive
+              ? "hamburger hamburger--spring is-active"
+              : "hamburger hamburger--spring "
+          }
+          onClick={toggleClass}
+          type="button"
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
       </nav>
     </div>
   );
