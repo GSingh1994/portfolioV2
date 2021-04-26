641771168af1b94e3ba1240062df823383f17b0c
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-04-25 22:35:15 -0500

    :lipstick: Fixed Footer responsiveness

--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -3,16 +3,15 @@ import logoLinkedin from "../assets/logo-linkedin.svg";
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="leftFooter">
-        Made with
-        <span className="leftIcons"> ☕</span>
-      </div>
-      <div className="rightFooter">
-        © 2021 Gagandeep Singh
+      <div className="copyRight">© 2021 Gagandeep Singh</div>
+
+      <div className="icons">
         <img src={logoGithub} alt="" />
         <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="" />
         <img src={logoLinkedin} alt="" />
       </div>
+
+      <div className="madeWith">Made with ☕</div>
     </div>
   );
 };
